8ff34e585118998b0817d70d1c120ce2b5850fec
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2022-10-07 11:45:19 +0200

    F #299: add datastore resource

--- opennebula/provider.go
+++ opennebula/provider.go
@@ -97,6 +97,7 @@ func Provider() *schema.Provider {
 			"opennebula_virtual_router":                   resourceOpennebulaVirtualRouter(),
 			"opennebula_virtual_router_nic":               resourceOpennebulaVirtualRouterNIC(),
 			"opennebula_virtual_network_address_range":    resourceOpennebulaVirtualNetworkAddressRange(),
+			"opennebula_datastore":                        resourceOpennebulaDatastore(),
 		},
 
 		ConfigureContextFunc: providerConfigure,
